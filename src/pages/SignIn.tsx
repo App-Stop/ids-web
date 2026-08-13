8ff52bad46d3eede83814643052d9de8d21c8049
hamzak22 <hkhamzakhan22@gmail.com>
2026-08-13 22:25:02 +0500

    build error fixed

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import AuthLayout from '../components/AuthLayout'
 import BrandLogos from '../components/BrandLogos'
 import EmailField, { DOMAIN } from '../components/EmailField'
